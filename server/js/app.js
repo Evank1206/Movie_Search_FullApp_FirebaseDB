e99d2397aa4ab3a614bea2031ea56e34136a87be
Jatinokov <Enkhtaivan1206@gmail.com>
2020-04-13 23:49:03 -0700

    initializing jQuery and function that triger the value for user input and console.logged in browser

--- server/js/app.js
+++ server/js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-    
+
+    $("#text-submit").on("click", function(){
+       var getfrom =  $("#user-text").val().trim();
+       console.log(getfrom);
+       
+        
+    })
 
 
 });
