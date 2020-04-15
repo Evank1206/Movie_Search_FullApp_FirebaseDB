79e33abef3a76944d8b0f053bf839b88d428d068
Jatinokov <Enkhtaivan1206@gmail.com>
2020-04-15 02:13:18 -0700

    in client: in html: set up semetic.ui cdn & app layout with semetic.ui, in server: in app.js: created function that render button with grabbingthe value from user input

--- server/js/app.js
+++ server/js/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
 
-    $("#text-submit").on("click", function(){
-       var getfrom =  $("#user-text").val().trim();
-       console.log(getfrom);
-       
-        
+    $(".button").on("click", function (e) {
+        e.preventDefault()
+        var getUserData = $(".input").val().trim();
+        console.log(getUserData);
+
+        var btn = $("<button>");
+            btn.addClass("ui button inverted primary createdBtn");
+            btn.attr("btnAttr");
+            btn.text(getUserData)
+        $(".btn-View").append(btn)
+
+
+        // $(".btn-View").append(getUserData);
+
     })
-
-
 });
