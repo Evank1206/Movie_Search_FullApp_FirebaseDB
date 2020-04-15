fcc50e7ce5e3e1aac8c6a0cdda39d397d7a9c80e
Jatinokov <Enkhtaivan1206@gmail.com>
2020-04-15 02:25:02 -0700

    in js: gave conditional statement for while userInput empty user clicked the search btn it'll alert to user enter value

--- server/js/app.js
+++ server/js/app.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
+    // When user click the btn grab the value from User Input // showed in console.log
+
 
     $(".button").on("click", function (e) {
         e.preventDefault()
         var getUserData = $(".input").val().trim();
         console.log(getUserData);
+        if (getUserData == 0) {
+            alert("Enter The Movie Name!")
+        } else {
+        //  creating the btn dynamic way to render them into html 
+            var btn = $("<button>");
+                btn.addClass("ui button inverted primary createdBtn");
+                btn.attr("btnAttr");
+                btn.text(getUserData);
+            $(".btn-View").append(btn);
+        }
 
-        var btn = $("<button>");
-            btn.addClass("ui button inverted primary createdBtn");
-            btn.attr("btnAttr");
-            btn.text(getUserData)
-        $(".btn-View").append(btn)
 
-
-        // $(".btn-View").append(getUserData);
 
     })
 });
