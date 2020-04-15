77065867c3df4b494894071b8e5e6f95ae3adc97
Jatinokov <Enkhtaivan1206@gmail.com>
2020-04-15 02:43:02 -0700

    in js: set up the movie url & initialized in variable, the using ajax for pull the data from url

--- server/js/app.js
+++ server/js/app.js
@@ -16,8 +16,27 @@ $(document).ready(function () {
                 btn.text(getUserData);
             $(".btn-View").append(btn);
         }
+        // when clicked on created btn console.log the message
+        $(".createdBtn").on("click", function(){
+            
+            var hardCodedMovie = "Love";
+            var movieURL = "https://www.omdbapi.com/?t="+hardCodedMovie+"&apikey=trilogy";
+            
+            $.ajax({
+                type: "GET",
+                url: movieURL
+            }).then(function(err, data){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(data);
+                }
+            })
 
+            
+        })
 
+    });
 
-    })
+   
 });
